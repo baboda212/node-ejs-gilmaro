24f7785bb9a0dadd5dc693bdc33c521ca661576d
baboda212 <baboda212@naver.com>
2023-01-10 19:16:40 +0900

    슬라이드쇼 변경

--- public/js/slide.js
+++ public/js/slide.js
@@ -8,46 +8,25 @@ document.addEventListener('DOMContentLoaded', function(e) {
   }); 
 
 /* 슬라이드쇼 */
-let curPos = 0;
-let postion = 0;
-let start_x, end_x;
-const IMAGE_WIDTH = 375;
-const images = document.querySelector(".images") 
- 
-images.addEventListener('touchstart', touch_start);
-images.addEventListener('touchmove', touch_move);
-images.addEventListener('touchend', touch_end);
- 
-function prev(){
-  if(curPos > 0){
-    postion += IMAGE_WIDTH;
-    images.style.transform = `translateX(${postion}px)`;
-    curPos = curPos - 1;
-  }
-}
-function next(){
-  if(curPos < 4){
-    postion -= IMAGE_WIDTH;
-    images.style.transform = `translateX(${postion}px)`;
-    curPos = curPos + 1;
-  }
-}
- 
-function touch_start(event) {
-  start_x = event.touches[0].pageX
-}
-function touch_move(event) {
-  start_x = event.touches[0].pageX
-}
- 
-function touch_end(event) {
-  end_x = event.changedTouches[0].pageX;
-  if(start_x > end_x){
-    next();
-  }else{
-    prev();
-  }
-}
+$(function () {
+  let bgImg = [
+    "images/visual01.jpg",
+    "images/visual02.jpg",
+    "images/visual03.jpg",
+    "images/visual04.jpg",
+    "images/visual05.jpg",
+  ];
+  let bgImgNum = 0;
+  setInterval(function () {
+    bgImgNum++;
+    if (bgImgNum >= bgImg.length) {
+      bgImgNum = 0;
+    }
+    $(".main-visual").css({
+      "background-image": `url(${bgImg[bgImgNum]})`,
+    });
+  }, 5000);
+});
 
 /* 좋아요하트 */
 $(function(){
