64cca41ece5efe10ae6e3f430d573a85adbc09dd
baboda212 <baboda212@naver.com>
2023-01-11 10:41:49 +0900

    일부 수정

--- public/js/sitemap.js
+++ public/js/sitemap.js
@@ -14,3 +14,20 @@ $('.container .site-map .site-map-header .title a').on('click', function(){
 $('.container .site-map .site-map-nav .depth1-title').on('click', function(){
     $(this).next().toggleClass('show')
 })
+
+/* 바로구매하기 버튼 클릭시 */
+$('.container .cashier-btn .buy-btn').on('click', function(){
+    $('.container .cashier').addClass('show');
+})
+
+$('.container .cashier .cashier-close').on('click', function(){
+    $('.container .cashier').removeClass('show');
+})
+
+/* bottom-nav 클릭시 */
+$('.bottom-nav').on('click', function(){
+    $(this).css('bottom', '0');
+    $('.container .side-btn').css('bottom', '180px');
+    $('.container .cashier-btn').css('bottom', '70px');
+    $('.container .cashier').css('bottom', '150px');
+})
