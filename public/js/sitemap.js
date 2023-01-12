794192b8d376820298b0e165a0bbe55e443e4779
baboda212 <baboda212@naver.com>
2023-01-12 19:32:33 +0900

    pay.ejs 하단 메뉴 수정완료

--- public/js/sitemap.js
+++ public/js/sitemap.js
@@ -1,35 +1,35 @@
 /* bottom-nav 메뉴 클릭시 */
-$('.container .bottom-nav ul #menu a').on('click', function(){
-    $('.container .site-map').addClass('show')
+$('.bottom-nav ul #menu a').on('click', function(){
+    $('.site-map').addClass('show')
 })
 
 
 /* site-map 닫기버튼 클릭시 */
-$('.container .site-map .site-map-header .title a').on('click', function(){
-    $('.container .site-map').removeClass('show')
+$('.site-map .site-map-header .title a').on('click', function(){
+    $('.site-map').removeClass('show')
 })
 
 
 /* site-map depth1 클릭시 */
-$('.container .site-map .site-map-nav .depth1-title').on('click', function(){
+$('.site-map .site-map-nav .depth1-title').on('click', function(){
     $(this).next().toggleClass('show')
 })
 
 /* 바로구매하기 버튼 클릭시 */
-$('.container .cashier-btn .buy-btn').on('click', function(){
-    $('.container .cashier').addClass('show');
+$('.cashier-btn .buy-btn').on('click', function(){
+    $('.cashier').addClass('show');
 })
 
-$('.container .cashier .cashier-close').on('click', function(){
-    $('.container .cashier').removeClass('show');
+$('.cashier .cashier-close').on('click', function(){
+    $('.cashier').removeClass('show');
 })
 
 /* bottom-nav 클릭시 */
 $('.bottom-nav').on('click', function(){
     $(this).css('bottom', '0');
     $(this).css('backgroundColor', '#fff');
-    $('.container .side-btn').css('bottom', '180px');
-    $('.container .cashier-btn').css('bottom', '70px');
-    $('.container .cashier').css('bottom', '150px');
+    $('.side-btn').css('bottom', '180px');
+    $('.cashier-btn').css('bottom', '70px');
+    $('.cashier').css('bottom', '150px');
 })
 
